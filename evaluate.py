fc477e9a887c28ae5af4bbd29ac49186cc3680d3
tjuzqc <cszqc@tju.edu.cn>
2021-01-13 17:40:24 +0800

    新增查看kappa指标

--- evaluate.py
+++ evaluate.py
@@ -3,72 +3,135 @@
 Author: TJUZQC
 Date: 2020-09-15 17:20:20
 LastEditors: TJUZQC
-LastEditTime: 2020-11-20 19:24:15
+LastEditTime: 2021-01-13 17:35:58
 Description: None
 '''
-import torch
-from torch import from_numpy
-from torch.autograd import Function
+import numpy as np
 
 
-class DiceCoeff(Function):
-    """Dice coeff for individual examples"""
+def calculate_area(pred, label, num_classes, ignore_index=255):
+    """
+    Calculate intersect, prediction and label area
 
-    def forward(self, input, target):
-        self.save_for_backward(input, target)
-        eps = 0.0001
-        self.inter = torch.dot(input.view(-1), target.view(-1))
-        self.union = torch.sum(input) + torch.sum(target) + eps
-        t = (2 * self.inter.float() + eps) / self.union.float()
-        return t
+    Args:
+        pred (Tensor): The prediction by model.
+        label (Tensor): The ground truth of image.
+        num_classes (int): The unique number of target classes.
+        ignore_index (int): Specifies a target value that is ignored. Default: 255.
 
-    # This function has only a single output, so it gets only one gradient
-    def backward(self, grad_output):
+    Returns:
+        Tensor: The intersection area of prediction and the ground on all class.
+        Tensor: The prediction area on all class.
+        Tensor: The ground truth area on all class
+    """
+    if len(pred.shape) == 2:
+        pred = pred[np.newaxis, :, :]
+    if len(label.shape) == 2:
+        label = label[np.newaxis, :, :]
+    if not pred.shape == label.shape:
+        raise ValueError('Shape of `pred` and `label should be equal, '
+                         'but there are {} and {}.'.format(
+                             pred.shape, label.shape))
 
-        input, target = self.saved_variables
-        grad_input = grad_target = None
+    # Delete ignore_index
+    mask = label != ignore_index
+    pred = pred + 1
+    label = label + 1
+    pred = pred * mask
+    label = label * mask
 
-        if self.needs_input_grad[0]:
-            grad_input = grad_output * 2 * (target * self.union - self.inter) \
-                / (self.union * self.union)
-        if self.needs_input_grad[1]:
-            grad_target = None
+    pred = np.eye(num_classes + 1)[pred] # F.one_hot(pred, num_classes + 1)
+    label = np.eye(num_classes + 1)[label] # F.one_hot(pred, num_classes + 1)
+    pred = pred[:, :, :, 1:]
+    label = label[:, :, :, 1:]
 
-        return grad_input, grad_target
+    pred_area = []
+    label_area = []
+    intersect_area = []
 
+    for i in range(num_classes):
+        pred_i = pred[:, :, :, i]
+        label_i = label[:, :, :, i]
+        pred_area_i = np.sum(pred_i)
+        label_area_i = np.sum(label_i)
+        intersect_area_i = np.sum(pred_i * label_i)
+        pred_area.append(pred_area_i)
+        label_area.append(label_area_i)
+        intersect_area.append(intersect_area_i)
+    pred_area = np.array(pred_area)
+    label_area = np.array(label_area)
+    intersect_area = np.array(intersect_area)
+    return intersect_area, pred_area, label_area
 
-def dice_coeff(input, target):
-    """Dice coeff for batches"""
-    input, target = from_numpy(input).squeeze(0), from_numpy(target).squeeze(0)
-    if input.max() == 255.:
-        input = input.div(255.)
-    if target.max() == 255.:
-        target = target.div(255.)
+class Matrix(object):
+    def __init__(self, input, target) -> None:
+        super().__init__()
+        input, target = np.array(input, dtype=np.int32), np.array(target, dtype=np.int32)
+        self.intersect_area, self.pred_area, self.label_area = calculate_area(input, target, len(np.unique(target)))
 
-    return DiceCoeff().forward(input, target)
+    def __call__(self):
+        return self.miou(), self.accuracy(),self.kappa()
 
+    def miou(self):
+        """
+        Calculate iou.
 
-class PixelAccuracy(Function):
-    """Pixel accuracy for individual examples"""
+        Args:
+            intersect_area (Tensor): The intersection area of prediction and ground truth on all classes.
+            pred_area (Tensor): The prediction area on all classes.
+            label_area (Tensor): The ground truth area on all classes.
 
-    def forward(self, input, target):
-        assert input.shape == target.shape, f'the size of input and target must be same'
-        tmp = input == target
-        acc = torch.sum(tmp).float() / input.nelement()
-        # acc = np.sum(input[target == input])/np.sum(input)
-        return acc
+        Returns:
+            np.ndarray: iou on all classes.
+            float: mean iou of all classes.
+        """
+        union = self.pred_area + self.label_area - self.intersect_area
+        class_iou = []
+        for i in range(len(self.intersect_area)):
+            if union[i] == 0:
+                iou = 0
+            else:
+                iou = self.intersect_area[i] / union[i]
+            class_iou.append(iou)
+        miou = np.mean(class_iou)
+        return np.array(class_iou), miou
+    
+    def accuracy(self):
+        """
+        Calculate accuracy
 
+        Args:
+            intersect_area (Tensor): The intersection area of prediction and ground truth on all classes..
+            pred_area (Tensor): The prediction area on all classes.
 
-def pixel_accuracy(input, target):
-    """Pixel accuracy for batches"""
-    input, target = from_numpy(input).squeeze(0), from_numpy(target).squeeze(0)
-    if input.max() == 255.:
-        input = input.div(255.)
-    if target.max() == 255.:
-        target = target.div(255.)
+        Returns:
+            np.ndarray: accuracy on all classes.
+            float: mean accuracy.
+        """
+        class_acc = []
+        for i in range(len(self.intersect_area)):
+            if self.pred_area[i] == 0:
+                acc = 0
+            else:
+                acc = self.intersect_area[i] / self.pred_area[i]
+            class_acc.append(acc)
+        macc = np.sum(self.intersect_area) / np.sum(self.pred_area)
+        return np.array(class_acc), macc
 
-    return PixelAccuracy().forward(input, target)
+    def kappa(self):
+        """
+        Calculate kappa coefficient
 
+        Args:
+            intersect_area (Tensor): The intersection area of prediction and ground truth on all classes..
+            pred_area (Tensor): The prediction area on all classes.
+            label_area (Tensor): The ground truth area on all classes.
 
-def Mask2Tensor(input):
-    return from_numpy(input).div(255)
+        Returns:
+            float: kappa coefficient.
+        """
+        total_area = np.sum(self.label_area)
+        po = np.sum(self.intersect_area) / total_area
+        pe = np.sum(self.pred_area * self.label_area) / (total_area * total_area)
+        kappa = (po - pe) / (1 - pe)
+        return kappa
